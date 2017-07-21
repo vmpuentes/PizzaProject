1fb58a04e6279569ebe687ff97fd553a74e042e2
Victor <vmpuentes@gmail.com>
2017-07-21 16:45:40 -0700

    add background image to jumbotron.

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+//business logic
+
+function Pizza () {
+  this.pizzaNew = new Pizza("pizzaNew");
+  this.toppings = ["provolone", "crickets", "wasabi", "jalapenos", "pepperoni"."pepperoncinis","chicharonnis","canadadian bacon","mustard"];
+  this.price = ["$10","$15","$20"]
+  this.size = ["small","medium","large"];
+}
+
+function Customer () {
+  this.name = name;
+  cost = []
+}
+
+Pizza.prototype.orderPizza = function() {
+  if (this.pizzaNew.name === "pizzaNew") {
+    this.toppings + this.size = this.price
+  } else {
+    this.price = 0
+  }
+}
