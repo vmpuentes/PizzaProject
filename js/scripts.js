d36cae1c36548fe8f0a27ee7e616278a23f62c2f
Victor Puentes <vmpuentes@gmail.com>
2017-07-27 12:20:09 -0700

    add constructor to scripts.js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,50 @@
 //business logic
-
+function Customer (name,finalPrice) {
+  this.name = name;
+  this.finalPrice = finalPrice;
+}
 function Pizza (toppings,size) {
-  this.toppings = [];
+  this.toppings = toppings;
   this.price = 10;
   this.pizzaSize = size;
 }
 
 Pizza.prototype.calculatePizzaPrice = function() {
   if (this.pizzaSize === "medium") {
-  return (this.price * this.quantity + this.toppings.length) + 5;
+  return (this.price + this.toppings.length * .5) + 5;
   } else if (this.pizzaSize === "large") {
-  return (this.price * this.quantity + this.toppings.length) + 10;
+  return (this.price + this.toppings.length * .5) + 10;
 } else {
-return (this.price * this.quantity + this.toppings.length);
+return (this.price + this.toppings.length * .5);
+ }
 }
+console.log("hello");
+var pizza = new Pizza();
 
-function Pizza = orderPizza() {
-  $("form#calculate").val();
-  $("form-group.toppings");
-};
+// function Pizza = orderPizza() {
+//   $("form#calculate").val();
+//   $("form-group.toppings");
+// };
 //User Interface logic
 $(document).ready(function() {
   $("form#pizzaInput").submit(function(event) {
     event.preventDefault();
-    console.log("hello")
-    var pizza = new Pizza();
-     var newToppings = $("input:checkbox[name=toppings]:checked").val();     
-      $("input:checkbox[name=size]:checked").val();
-        $("button#calculate").append();
+    var inputtedName = $("#yourName").text();
+
+    // var pizza = new Pizza();
+
+    var size = $("#size").val();
+    //       var selectSize = $(this).val();
+
+    var toppings = $("input:checkbox[name=toppings]:checked").val();
+    //    var selectToppings = $(this).val();
+    //  });
+     //
+
+      $("button#calculate").append("<li>" +  + "</li>");
 
   });
 });
+//
+// var newToppings = $("input:checkbox[name=toppings]:checked").val();
+//  var newSize = $("input:checkbox[name=size]:checked").val();
